5027ea28a5b6ea773fc3af13fe48a7f4953ae97f
Kayla Z <kaylaz98@berkeley.edu>
2023-05-17 00:25:43 -0700

    fixed hourly data

--- src/HourlyNextDay.js
+++ src/HourlyNextDay.js
@@ -17,35 +17,67 @@ function HourlyNextDay({ lat, lon }) {
     }, [lat, lon]);
 
     if (!data) { return null }
+
     return (
         <>
             <h2 className="title">Weather for the Next 24 Hours</h2>
-            {data.slice(0, 24).map((hour, index) => {
-                return (
-                    <>
-
-                        <center>
-                            <img
-                                src={`https://openweathermap.org/img/wn/${hour.weather[0].icon}@2x.png`}
-                                alt="centered image"
-                                height="300"
-                                width="300"
-                                className="image"
-                            />
-                        </center>
-                        <p>Time: {new Date(hour.dt * 1000).toLocaleString('en-US', { timeZone: 'America/Los_Angeles', timeStyle: 'short' })}</p>
-                        <p>Temperature: {hour.temp}°F</p>
-                        <p>Humidity: {hour.humidity}%</p>
-                        <p>Average visibility: {hour.visibility} km</p>
-                        <p>Wind speed: {hour.wind_speed} miles/hour</p>
-                        <p>Wind direction: {hour.wind_deg} degrees</p>
-                        <p>Cloudiness: {hour.clouds}%</p>
-                        <p>UV index: {hour.uvi}</p>
-                    </>
-                );
-            })}
+            <div className="container-hourly">
+                {data.slice(0, 24).map((hour, index) => {
+                    return (
+                        <div className="item-hourly" key={index}>
+                            <center>
+                                <img
+                                    src={`https://openweathermap.org/img/wn/${hour.weather[0].icon}@2x.png`}
+                                    alt="centered image"
+                                    height="300"
+                                    width="300"
+                                    className="image"
+                                />
+                            </center>
+                            <p>Time: {new Date(hour.dt * 1000).toLocaleString('en-US', { timeZone: 'America/Los_Angeles', timeStyle: 'short' })}</p>
+                            <p>Temperature: {hour.temp}°F</p>
+                            <p>Humidity: {hour.humidity}%</p>
+                            <p>Average visibility: {hour.visibility} km</p>
+                            <p>Wind speed: {hour.wind_speed} miles/hour</p>
+                            <p>Wind direction: {hour.wind_deg} degrees</p>
+                            <p>Cloudiness: {hour.clouds}%</p>
+                            <p>UV index: {hour.uvi}</p>
+                        </div>
+                    );
+                })}
+            </div>
         </>
     );
+
+    // return (
+    //     <>
+    //         <h2 className="title">Weather for the Next 24 Hours</h2>
+    //         {data.slice(0, 24).map((hour, index) => {
+    //             return (
+    //                 <>
+
+    //                     <center>
+    //                         <img
+    //                             src={`https://openweathermap.org/img/wn/${hour.weather[0].icon}@2x.png`}
+    //                             alt="centered image"
+    //                             height="300"
+    //                             width="300"
+    //                             className="image"
+    //                         />
+    //                     </center>
+    //                     <p>Time: {new Date(hour.dt * 1000).toLocaleString('en-US', { timeZone: 'America/Los_Angeles', timeStyle: 'short' })}</p>
+    //                     <p>Temperature: {hour.temp}°F</p>
+    //                     <p>Humidity: {hour.humidity}%</p>
+    //                     <p>Average visibility: {hour.visibility} km</p>
+    //                     <p>Wind speed: {hour.wind_speed} miles/hour</p>
+    //                     <p>Wind direction: {hour.wind_deg} degrees</p>
+    //                     <p>Cloudiness: {hour.clouds}%</p>
+    //                     <p>UV index: {hour.uvi}</p>
+    //                 </>
+    //             );
+    //         })}
+    //     </>
+    // );
 
 
 
